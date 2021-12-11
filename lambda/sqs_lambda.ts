d5f7448df692693d7f81a006b626219b8a21cd4d
Martin <mart256@gmail.com>
2021-12-11 23:15:16 +0100

    Finished logic of receiver lambda, deciding what happens on order which is not already processed, it will throw an error, which means the SQS message will go back to the queue and will retry again, 5 times before going to the DQL. It will wait 30 seconds before trying again (visibility timeout).

--- lambda/sqs_lambda.ts
+++ lambda/sqs_lambda.ts
@@ -1,6 +1,7 @@
-import {SQS} from 'aws-sdk'
+import {SQS, DynamoDB} from 'aws-sdk'
 
 const sqs = new SQS({apiVersion: '2012-11-05'});
+const dynamo = new DynamoDB.DocumentClient();
 
 interface EventCreate {
     body: string;
@@ -9,6 +10,21 @@ interface EventCreate {
 interface Order {
     id: number;
     description: string;
+    isProcessed: boolean;
+}
+
+interface EventSQS {
+    Records: SQSRecord[]
+}
+
+interface SQSRecord {
+    body: string,
+    messageAttributes: MessageAttributes
+}
+
+interface MessageAttributes {
+    OrderId: { stringValue: string };
+    Description: { stringValue: string };
 }
 
 exports.emitter = async (event: EventCreate) => {
@@ -37,8 +53,34 @@ exports.emitter = async (event: EventCreate) => {
     return sendRes(200, "Order created!");
 };
 
-exports.receiver = async (event: any) => {
+exports.receiver = async (event: EventSQS) => {
     console.log(event);
+    const { Records: [firstRecord] } = event;
+    const { messageAttributes: { Description, OrderId: {stringValue: orderId} } } = firstRecord;
+    console.log(Description.stringValue);
+
+    const dbQuery = {
+        TableName: process.env.TABLE_NAME!,
+        KeyConditionExpression: "#keyName = :orderId",
+        ExpressionAttributeNames:{
+            "#keyName": "id"
+        },
+        ExpressionAttributeValues: {
+            ":orderId": orderId
+        }
+    };
+
+    const queryResult = await dynamo.query(dbQuery).promise();
+    const { Count = 0, Items } = queryResult;
+    if (Count > 0) {
+        const order: Order = Items?.[0] as Order;
+        if (!order.isProcessed) {
+            throw new Error("Order was not processed, trying later.");
+        } else {
+            console.log(`Order ${orderId} was processed.`);
+        }
+    }
+
     return "Order Event received!";
 };
 
